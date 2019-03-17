7d375d04bb58f6faaca6789c74738f68d821e436
Wenjun Huang <55865950@qq.com>
2019-03-17 16:02:13 +0800

    JFX-Scala build ui:finish ui

--- Calculator/JFX-Scala/src/main/scala/com/github/wenjunhuang/uiapps/calculator/Calculator.scala
+++ Calculator/JFX-Scala/src/main/scala/com/github/wenjunhuang/uiapps/calculator/Calculator.scala
@@ -6,21 +6,20 @@ import scalafx.application.JFXApp
 import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.Scene
 import scalafx.scene.control.{Button, TextField}
-import scalafx.scene.layout.{Border, GridPane}
+import scalafx.scene.layout.{Border, GridPane, Region}
 import scalafx.scene.text.Font
+import javafx.scene.control.{Button => jfxButton}
 
 object Calculator extends JFXApp {
   val kNumDigitButtons = 10
 
   stage = new application.JFXApp.PrimaryStage {
     title.value = "Calculator"
-//    resizable = false
     scene = new Scene {
       content = new GridPane {
         hgap = 9.0
         vgap = 9.0
         padding = Insets(11.0)
-        style = "-fx-border-color:black"
 
         val display = new TextField {
           text = "0"
@@ -32,9 +31,9 @@ object Calculator extends JFXApp {
         GridPane.setConstraints(display, 0, 0, 6, 1)
         children += display
 
-        val digitButtons = (0 to kNumDigitButtons).map(num => createButton(num.toString, digitClicked))
+        val digitButtons = (0 until kNumDigitButtons).map(num => createButton(num.toString, digitClicked))
         val pointButton = createButton(".", pointClicked)
-        val changeSignButton = createButton("\302\261", changeSignClicked)
+        val changeSignButton = createButton("\u00B1", changeSignClicked)
 
         val backspaceButton = createButton("Backspace", backspaceClicked)
         val clearButton = createButton("Clear", clearClicked)
@@ -64,29 +63,65 @@ object Calculator extends JFXApp {
         GridPane.setConstraints(clearAllButton, 4, 1, 2, 1)
         children += clearAllButton
 
-        GridPane.setConstraints(clearMemoryButton,0,2)
+        GridPane.setConstraints(clearMemoryButton, 0, 2)
         children += clearMemoryButton
 
-        GridPane.setConstraints(readMemoryButton,0,3)
+        GridPane.setConstraints(readMemoryButton, 0, 3)
         children += readMemoryButton
 
-        GridPane.setConstraints(setMemoryButton,0,4)
+        GridPane.setConstraints(setMemoryButton, 0, 4)
         children += setMemoryButton
 
-        GridPane.setConstraints(addToMemoryButton,0,5)
+        GridPane.setConstraints(addToMemoryButton, 0, 5)
         children += addToMemoryButton
 
+        (1 until kNumDigitButtons).foreach { i =>
+          val row = ((9 - i) / 3) + 2
+          val column = ((i - 1) % 3) + 1
+          children += digitButtons(i)
+          GridPane.setConstraints(digitButtons(i), column, row)
+        }
+        children += digitButtons(0)
+        GridPane.setConstraints(digitButtons(0), 1, 5)
 
-        println(s"width:${addToMemoryButton.getPrefWidth},height:${addToMemoryButton.getPrefHeight}")
-        println(delegate.getColumnCount)
+        children += pointButton
+        GridPane.setConstraints(pointButton, 2, 5)
+
+        children += changeSignButton
+        GridPane.setConstraints(changeSignButton, 3, 5)
+
+        children += divisionButton
+        GridPane.setConstraints(divisionButton, 4, 2)
+
+        children += timesButton
+        GridPane.setConstraints(timesButton, 4, 3)
+
+        children += minusButton
+        GridPane.setConstraints(minusButton,4,4)
+
+        children += plusButton
+        GridPane.setConstraints(plusButton,4,5)
+
+        children += squareRootButton
+        GridPane.setConstraints(squareRootButton,5,2)
+
+        children += powerButton
+        GridPane.setConstraints(powerButton,5,3)
+
+        children += reciprocalButton
+        GridPane.setConstraints(reciprocalButton,5,4)
+
+        children += equalButton
+        GridPane.setConstraints(equalButton,5,5)
       }
     }
+    resizable = false
   }
 
   def createButton(buttonText: String, clicked: Button => Unit): Button = {
-    new Button { self =>
+    new Button(new CalculatorButton) {
+      self =>
       text = buttonText
-//      prefHeight = prefHeight.value + 20.0
       onAction = { _ =>
         clicked(self)
       }
@@ -107,22 +142,33 @@ object Calculator extends JFXApp {
 
   def clearMemoryClicked(button: Button) = ???
 
-  def addToMemoryClicked(button:Button) = ???
+  def addToMemoryClicked(button: Button) = ???
 
-  def multiplicativeOperatorClicked(button:Button) = ???
+  def multiplicativeOperatorClicked(button: Button) = ???
 
-  def unaryOperatorClicked(button:Button) = ???
+  def unaryOperatorClicked(button: Button) = ???
 
-  def equalClicked(button:Button) = ???
+  def equalClicked(button: Button) = ???
 
-  def readMemoryClicked(button:Button) = ???
+  def readMemoryClicked(button: Button) = ???
 
-  def setMemoryClicked(button:Button) = ???
+  def setMemoryClicked(button: Button) = ???
 
-  def additiveOperatorClicked(button:Button) = ???
+  def additiveOperatorClicked(button: Button) = ???
 }
 
 
-class CalculatorButton extends Button {
+class CalculatorButton extends jfxButton {
+  setMaxSize(Double.MaxValue, Double.MaxValue)
+
+  override protected def computePrefHeight(v: Double): Double = {
+    val prefHeight = super.computePrefHeight(v)
+    prefHeight + 20.0
+  }
+
+  override protected def computePrefWidth(v: Double): Double = {
+    val prefWidth = super.computePrefWidth(v)
+    prefWidth + 20.0
+  }
 
 }
